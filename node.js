ccc42ec032c7e87f8c5ff5e9275ccdb0152b518b
bet476 <60711188+bet476@users.noreply.github.com>
2020-05-25 22:27:03 -0300

    Add files via upload

---
+++ node.js
@@ -0,0 +1,14 @@
+fetch("https://content-asae1-up-4.uplynk.com/channel/7889811e7efb47f4a012cfba6ed4f2f9.m3u8?exp=1590456456&ct=c&cid=7889811e7efb47f4a012cfba6ed4f2f9&expand=drmOff&iph=0946dc5ce8f0b26038428abfdefb2df36fe240880805203313bc1539565c9ca2&rays=edcba&euid=%7BDC563C56-E926-474B-8583-1EEAFF342003%7D_000_1_001_live_00-06-00_1.2.2.23&cdn=ec&stgcfg=datg&pp2ip=0&delay=0&v=3&sig=98bc5b964e09544465ac07989c812fb53eaf7c5d2b9dee8ab59abed3e5f75df2&pbs=e4ab396e95854ed4976159c2784b56d2", {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "cross-site"
+  },
+  "referrer": "https://www.foxplay.com/pt-br/watch-live/foxsportsbrasil1",
+  "referrerPolicy": "no-referrer-when-downgrade",
+  "body": null,
+  "method": "GET",
+  "mode": "cors"
+});
